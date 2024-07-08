960990c4b1aaa7c453f0bc562c68e8e1c9ceb7b3
GunixYT <gammerpro06@gmail.com>
2024-07-07 20:45:28 -0500

    qwe

---
+++ src/components/Redes.tsx
@@ -0,0 +1,23 @@
+export default function Redes() {
+  return (
+    <div class='h-screen flex items-center justify-center fixed z-50'>
+      <div class=' bg-gray-900 rounded-xl grid grid-rows-5 gap-2 lg:gap-4 py-4 px-2'>
+        <a>
+          <img src='logos/Facebook.svg' alt='Facebook' class='h-6 w-6 lg:w-12 lg:h-12 hover:ml-1 duration-300' />
+        </a>
+        <a>
+          <img src='logos/Instagram.svg' alt='Instagram' class='h-6 w-6 lg:w-12 lg:h-12 hover:ml-1 duration-300' />
+        </a>
+        <a>
+          <img src='logos/Twitter.svg' alt='Twitter' class='h-6 w-6 lg:w-12 lg:h-12 hover:ml-1 duration-300' />
+        </a>
+        <a>
+          <img src='logos/Whatsapp.svg' alt='Whatsapp' class='h-6 w-6 lg:w-12 lg:h-12 hover:ml-1 duration-300' />
+        </a>
+        <a>
+          <img src='logos/Youtube.svg' alt='Youtube' class='h-6 w-6 lg:w-12 lg:h-12 hover:ml-1 duration-300' />
+        </a>
+      </div>
+    </div>
+  );
+}
